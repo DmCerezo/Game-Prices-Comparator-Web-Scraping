41bf3187a8924879724bba830dd913f15d3fc3f9
David MC <127233730+DmCerezo@users.noreply.github.com>
2024-05-21 04:52:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def juego(nombre):
     return f"Este es el juego {nombre}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5555, debug=True)
+  app.run(host="127.0.0.1", port=8080, debug=True)
