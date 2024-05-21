ecbcd0503e3bd43437d035bec2d8679f8e593b00
David MC <127233730+DmCerezo@users.noreply.github.com>
2024-05-21 04:01:53 +0200

    Aplicacion basica
    Web scraping de ejemplo funcionando

--- README.md
+++ README.md
@@ -2,10 +2,18 @@
 
 ## Como instalar y ejecutar
 
-Make sure you have the latest version of Python installed.
+Importante tener python instalado
+
+```bash
+pip --version
+```
 
 ```bash
 git clone https://github.com/DmCerezo/Proyecto-Web-Scraping-ISI-UGR.git
+```
+## Abrir el entorno de trabajo
+```bash
+.\env\Scripts\activate 
 ```
 
 ```bash
@@ -15,15 +23,9 @@ pip install -r requirements.txt
 ## Encender la aplicacion
 
 ```bash
-python main.py
+python app.py
 ```
 
 ## Para ver la aplicacion en local
+Go to `http://127.0.0.1:5555`
 
-Go to `http://127.0.0.1:5000`
-
-## Notas de desarrollo
-16/05/2024 David: 
-He dejado hecho la pagina basica y los script que son capaces de hacer webscraping para obtener el precio de los videojuegos en gog y steam eso si teniendo la URL 
-
-He intentado mostrar el precio de estos en la aplicion en la parte html todavia sin exito
---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template
+import re
+import json
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__, template_folder='templates')
+
+# Obtener precio de Fallout 4 en Steam
+def obtener_precio_fallout_4_STEAM():
+    url = "https://store.steampowered.com/app/377160/Fallout_4/"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        precio_element = soup.find("div", class_="game_purchase_price")
+        
+        if precio_element:
+            precio = precio_element.get_text().strip()
+            return precio
+        else:
+            return "Precio no encontrado"
+    else:
+        return f"Error al obtener la página: {response.status_code}"
+
+# Obtener precio de Fallout 4 en GOG
+def obtener_precio_fallout_4_GOG():
+    url = "https://www.gog.com/en/game/fallout_4_game_of_the_year_edition"
+    result = requests.get(url)
+    
+    if result.status_code == 200:
+        content = result.text
+        patron = r'"price":{([^}]+)}'
+        precio_match = re.search(patron, content)
+        
+        if precio_match:
+            precio_info_html = "{" + precio_match.group(1) + "}"
+            precio_info = json.loads(precio_info_html)
+            precio_fallout_4_GOG = precio_info.get("finalAmount")
+            return precio_fallout_4_GOG
+        else:
+            return "Precio no encontrado"
+    else:
+        return f"Error al obtener la página: {result.status_code}"
+
+@app.route('/')
+def index():
+    precio_fallout_4_steam = obtener_precio_fallout_4_STEAM()
+    precio_fallout_4_gog = obtener_precio_fallout_4_GOG()
+    return render_template('index.html', precio_fallout_4_steam=precio_fallout_4_steam, precio_fallout_4_gog=precio_fallout_4_gog)
+
+@app.route('/buscar_juegos')
+def buscar():
+    return render_template('buscar_juegos.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+@app.route('/juego/<nombre>')
+def juego(nombre):
+    return f"Este es el juego {nombre}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5555, debug=True)
